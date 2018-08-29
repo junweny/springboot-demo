06694d142d53f90707b27d86c17e949c720d1d78
junweny <32119055+junweny@users.noreply.github.com>
2018-08-29 21:17:23 +0800

    修改

--- springboot/src/main/java/com/yang/test/ApplicationDemo.java
+++ springboot/src/main/java/com/yang/test/ApplicationDemo.java
@@ -1,11 +1,8 @@
 package com.yang.test;
 
-import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApplicationDemo {
 	
 	public static void main(String[] args) throws Exception {
-        System.out.println(" springApplication run !");
         //SpringApplication springApplication = new SpringApplication(ApplicationDemo.class);
         //禁止命令行设置参数
         //springApplication.setAddCommandLineProperties(false);
