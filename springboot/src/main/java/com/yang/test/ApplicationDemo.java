28c9595cd0b4b8701f4a5418043b55f7740fed49
junweny <32119055+junweny@users.noreply.github.com>
2018-09-01 09:45:26 +0800

    修改

--- springboot/src/main/java/com/yang/test/ApplicationDemo.java
+++ springboot/src/main/java/com/yang/test/ApplicationDemo.java
@@ -1,14 +1,12 @@
 package com.yang.test;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@EnableAutoConfiguration
 @EnableAsync
 @SpringBootApplication
 public class ApplicationDemo {
@@ -21,7 +19,9 @@ public class ApplicationDemo {
         SpringApplication.run(ApplicationDemo.class, args);
     }
 	
-	
+
+
+
 	@RequestMapping(value = "/yang")
     String home() {
         return "Hello World!";
--- springboot/src/test/java/com/demo/ext/Test01.java
+++
@@ -1,9 +0,0 @@
-package com.demo.ext;
-
-public class Test01 {
-	public static void main(String[] args) {
-		DeleteEnum d1 = DeleteEnum.DELETE;
-		DeleteEnum d2 = DeleteEnum.DELETE;
-		System.out.println(d1 == d2);
-	}
-}
